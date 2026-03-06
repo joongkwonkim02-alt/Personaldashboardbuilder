4cbd1010d1e45b133a2402934673c77518615172
joongkwonkim02-alt <joongkwonkim02@gmail.com>
2026-03-06 22:50:03 +0900

    Create base Widget component

---
+++ src/components/Widget.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Widget: React.FC = () => {
+    return (
+        <div className="widget">
+            <h1>Widget Component</h1>
+        </div>
+    );
+};
+
+export default Widget;
